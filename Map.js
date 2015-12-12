7b99f8231cad3848c36fc50160e281cf1319cf63
Chad Nielsen <chad.m.nielsen@gmail.com>
2015-12-12 08:55:17 -0800

    Add navigator

--- Map.js
+++ Map.js
@@ -1,10 +1,13 @@
 define([
     "dojo/_base/declare",
-    "esri/map"
-], function(declare, Map){
+    "esri/map",
+    "esri/toolbars/navigation"
+], function(declare, Map, Navigation){
     return declare(Map, {
+        navigator: null,
         constructor: function(div, options){
             console.log('constructor called');
+            this.navigator = new Navigation(this);
         }
     });
 });
