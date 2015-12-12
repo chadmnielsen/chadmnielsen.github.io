6d71a3ef82affc246ddc84401e951a6668b4584b
Chad Nielsen <chad.m.nielsen@gmail.com>
2015-12-12 09:09:37 -0800

    Update esrimap.js

--- esrimap.js
+++ esrimap.js
@@ -1,11 +1,12 @@
 var map;
 
-require(["./Map.js", "dojo/domReady!"], function(Map) {
+require(["./Map.js", "esri/toolbars/navigation", "dojo/domReady!"], function(Map, Navigation) {
   map = new Map("map", {
     basemap: "topo",  //For full list of pre-defined basemaps, navigate to http://arcg.is/1JVo6Wd
     center: [-122.45, 37.75], // longitude, latitude
     zoom: 13
   });
   console.dir(map);
-  map.navigator.activate("PAN");
+  console.log(map.navigator.isFirstExtent());
+  map.navigator.activate(Navigation.PAN);
 });
